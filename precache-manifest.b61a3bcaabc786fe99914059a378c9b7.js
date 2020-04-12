56d69e7b03cc2c171d40b5da9852209ef2191d59
Ben Cao <benjamin.cao88@gmail.com>
2020-04-12 14:31:27 -0400

    Updates

---
+++ precache-manifest.b61a3bcaabc786fe99914059a378c9b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cb6007c1cd90e6adf7bcc1fc9ecd90",
+    "url": "/chucknorrisjokes/index.html"
+  },
+  {
+    "revision": "a4f86a0c5a8be32cd29c",
+    "url": "/chucknorrisjokes/static/css/main.628ee606.chunk.css"
+  },
+  {
+    "revision": "5eb9798c4bf84db0cbb3",
+    "url": "/chucknorrisjokes/static/js/2.393f47ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chucknorrisjokes/static/js/2.393f47ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f86a0c5a8be32cd29c",
+    "url": "/chucknorrisjokes/static/js/main.aca0a71e.chunk.js"
+  },
+  {
+    "revision": "d0d35beee886bea9bab3",
+    "url": "/chucknorrisjokes/static/js/runtime-main.2cbbfc65.js"
+  }
+]);
